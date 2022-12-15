ac47600e8b4af69cfdf9ba53b38e5c87610749f3
lyubov <lyuba.yureva.99@mail.ru>
2022-12-15 20:37:59 +0300

    refresh token

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", router);
